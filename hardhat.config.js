c9799db212bbc7c2c58d0e977aeeb55697d74c66
Hvbris <44342309+hvbr1s@users.noreply.github.com>
2022-09-30 22:28:46 +0200

    Add files via upload

---
+++ hardhat.config.js
@@ -0,0 +1,17 @@
+require("@nomiclabs/hardhat-ethers");
+require("@nomicfoundation/hardhat-chai-matchers");
+require("@nomiclabs/hardhat-etherscan");
+require("dotenv").config()
+
+module.exports = {
+  solidity: "0.8.10",
+  networks: {
+    mumbai: {
+      url: process.env.TESTNET_RPC,
+      accounts: [process.env.PRIVATE_KEY]
+    },
+  },
+  etherscan: {
+    apiKey: process.env.POLYGONSCAN_API_KEY
+  }
+};
